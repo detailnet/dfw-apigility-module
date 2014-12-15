c425d1b4aa3573f64c58c80466a8348876951e5f
Ivan Wolf <iwolf@detailag.ch>
2014-12-15 17:23:23 +0100

    Began refactoring domain/module structure

---
+++ Domain/Repository/RepositoryInterface.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Application\Core\Domain\Repository;
+
+interface RepositoryInterface
+{
+//    public function findAll();
+//
+//    public function find($id);
+//
+//    /**
+//     * Find entities by a set of criteria.
+//     *
+//     * @param array $criteria
+//     * @param array|null $orderBy
+//     * @param int|null $limit
+//     * @param int|null $offset
+//     * @return array The objects.
+//     */
+//    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null);
+//
+//    /**
+//     * Find a single entity by a set of criteria.
+//     *
+//     * @param array $criteria
+//     * @param array|null $orderBy
+//     * @return object|null The entity instance or NULL if the entity can not be found.
+//     */
+//    public function findOneBy(array $criteria, array $orderBy = null);
+//
+//    public function removeAll();
+//
+//    public function size();
+//
+//    public function beginTransaction();
+//
+//    public function commitTransaction();
+//
+//    public function rollbackTransaction();
+}
