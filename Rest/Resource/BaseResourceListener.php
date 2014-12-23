1368e3cd01c696daeb9c01e520fc75f67fe344ce
Ivan Wolf <iwolf@detailag.ch>
2014-12-23 19:28:50 +0100

    Tweaked paging

--- Rest/Resource/BaseResourceListener.php
+++ Rest/Resource/BaseResourceListener.php
@@ -84,12 +84,19 @@ class BaseResourceListener extends AbstractResourceListener
     {
         $params = (array) ($event->getQueryParams() ?: array());
 
-        if (isset($params['page']) && isset($params['page_size'])) {
-            $params['limit'] = $params['page_size'];
-            $params['offset'] = ($params['page'] - 1) * $params['page_size'];
-
-            unset($params['page'], $params['page_size']);
+        if (!isset($params['page'])) {
+            $params['page'] = 1;
         }
+
+        if (!isset($params['page_size'])) {
+            /** @todo Get from settings (subscribe to getList.pre?) */
+            $params['page_size'] = 10;
+        }
+
+        $params['limit'] = $params['page_size'];
+        $params['offset'] = ($params['page'] - 1) * $params['page_size'];
+
+        unset($params['page'], $params['page_size']);
 
         $params = array_merge(
             $event->getRouteMatch()->getParams(),
